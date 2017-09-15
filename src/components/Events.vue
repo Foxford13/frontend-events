4d9cc228d26dd0a379b4dc3f0740a69ed30ec750
Jakub Dziekan <jdziek89@gmail.com>
2017-09-16 00:41:50 +0100

    initial commit

---
+++ src/components/Events.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="events">
+  <h1>Events</h1>
+  <p>Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home. Home.</p>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'events',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,60 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+import Events from './components/Events'
+import About from './components/About'
+
+
+Vue.use(VueResource)
+Vue.use(VueRouter)
+Vue.use(BootstrapVue);
+
+
+Vue.config.productionTip = false
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {path:'/', component: Events},
+    {path: '/about', component: About}
+  ]
+})
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  template: `
+  <div id="app">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item"><router-link to="/" class="nav-link">Home </router-link></li>
+            <li class="nav-item"><router-link to="/about" class="nav-link">About </router-link></li>
+
+
+          </ul>
+        </div>
+      </nav>
+  <router-view></router-view>
+  </div>
+  `
+
+}).$mount('#app')
